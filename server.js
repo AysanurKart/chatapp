bbf6e32ae76d9276e8af67b8989e73a8144d921c
AysanurKart <ayka21ab@student.cbs.dk>
2022-11-22 13:00:56 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+
+const app = express ();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// forbinder statisk mappe 
+app.use(express.static(path.join(__dirname, 'public')));
+
+// run når en client fobinder
+io.on('connection', socket => {
+    
+    //Welcomes current user 
+    socket.emit('message', 'Welcome to LiveChat!');
+
+    //broadcast when a user connects
+    socket.broadcast.emit();
+
+    io.emit('message', 'A user has joined the chat');
+
+    //runs when client disconnects
+    socket.on('disconnect',() => {
+        io.emit('message', 'A user has left the chat');
+    });
+})
+
+const PORT = 3000 || process.env.PORT;
+
+server.listen(PORT, () => console.log(`Server running on ${PORT}`));
